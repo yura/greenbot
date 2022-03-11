18c13420d5122a899c4bdb7b02c0c243f2e7b36e
Volkov Maxim <maxim.volkov.cv@gmail.com>
2022-03-12 02:41:32 +0300

    feat: Add scripts

---
+++ model/config.py
@@ -0,0 +1,18 @@
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description="GreenBot")
+    parser.add_argument("--dataset_path", default="./datasets/seven_plastics", type=str,
+                        help="path to dataset dir")
+    parser.add_argument("--mode", default="train", type=str,
+                        help="set script mode {train, test, predict} (default: train)")
+    parser.add_argument("--img_size", default=200, type=int,
+                        help="image resize dimensions (default: 200")
+    parser.add_argument("--batch-size", default=8, type=int,
+                        help="train batch size (default: 8)")
+    parser.add_argument("--workers", default=4, type=int,
+                        help="number of data loading workers (default: 4)")
+    args = parser.parse_args()
+    
+    return args
+
