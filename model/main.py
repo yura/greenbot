00d29c66f7f61c811c428f7d47df0cab559b3ba2
Volkov Maxim <maxim.volkov.cv@gmail.com>
2022-03-12 08:05:18 +0300

    fix: minor

--- model/main.py
+++ model/main.py
@@ -116,7 +116,8 @@ def main(cfg):
     # criterion
     loss_fn = nn.CrossEntropyLoss()
     # optimizer
-    optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+#    optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+    optimizer = optim.Adam(model.parameters(), lr=3e-2, betas=(0.9, 0.999))
     
     # decay LR by a factor 0.1 every 5 epochs
     exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
